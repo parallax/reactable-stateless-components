78f08d60e4c15528e274078c1ffa6d72c2a77de9
William <will@parall.ax>
2021-12-17 11:15:24 +0000

    second commit

---
+++ src/createDirectory.js
@@ -0,0 +1,8 @@
+// import * as path from 'path';
+
+// const createDirectory = (uri, componentName)=>{
+//     const rootDir =  path.join(__dirname, '/');
+//     console.log('--->',rootDir);
+// }
+
+// export default createDirectory;
